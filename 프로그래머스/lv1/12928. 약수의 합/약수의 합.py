d635f79bd71fbbff2eff3d3417a87beab0cd5162
H-Seung <114974542+H-Seung@users.noreply.github.com>
2023-02-19 16:03:33 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -8,11 +8,9 @@
 # print(solution(16))
 
 def solution(n):
-    result = 0
     li = []
     for i in range(1,int(n**0.5)+1):
         if n % i == 0:
-            result += i + n/i
             li.extend([i, n/i])
     return sum(list(set(li)))
 
