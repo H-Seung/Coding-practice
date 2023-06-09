a267ab88b14aeb751b7ba4b33c042b3fc6864b11
Seunghee Han <114974542+H-Seung@users.noreply.github.com>
2023-06-09 23:27:25 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,9 +1,15 @@
-import math
+# import math
+
+# def solution(n):
+#     sqrt = math.sqrt(n)
+#     if sqrt - int(sqrt) == 0:
+#         answer = int(math.pow(sqrt+1,2))
+#     else:
+#         answer = -1
+#     return answer
 
 def solution(n):
-    sqrt = math.sqrt(n)
-    if sqrt - int(sqrt) == 0:
-        answer = int(math.pow(sqrt+1,2))
-    else:
-        answer = -1
-    return answer
+    sqrt = n**(1/2)
+    if sqrt % 1 == 0:
+        return (sqrt+1)**2
+    return -1
