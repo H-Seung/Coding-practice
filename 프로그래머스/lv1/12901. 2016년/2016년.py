b50eed7a74c2f367e802127f1cbbc8124cec44a0
Seunghee Han <114974542+H-Seung@users.noreply.github.com>
2023-08-10 23:47:43 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -3,3 +3,9 @@ def solution(a, b):
     result = ['THU','FRI','SAT','SUN','MON','TUE','WED']
     n = (sum(M[:a-1])+b)%7
     return result[n]
+
+import datetime
+a=5 
+b=24
+t = 'MON TUE WED THU FRI SAT SUN'.split()
+print(t[datetime.datetime(2016, a, b).weekday()])
