ded4df898fef715b14e34812ddd2ec7b424b29ce
Seunghee Han <114974542+H-Seung@users.noreply.github.com>
2024-09-23 23:10:54 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    ab = str(a)+str(b)
+    ba = str(b)+str(a)
+
+    if int(ab) >= int(ba):
+        answer = int(ab)
+    else:
+        answer = int(ba)
+    
+    return answer
