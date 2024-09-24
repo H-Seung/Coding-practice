68fb8ebd24905c1025ecf163849e98093e63da0b
Seunghee Han <114974542+H-Seung@users.noreply.github.com>
2024-09-24 22:35:05 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,22 @@
+def solution(wallpaper):
+    
+    lux = float('inf')   
+    luy = float('inf')  
+    rdx = 0              
+    rdy = 0            
+       
+    i = 0
+    for s in wallpaper:
+        print("i : ", i)
+        ix_l = s.find('#')  # '#'의 인덱스를 찾음
+        ix_r = s.rfind('#')
+        if ix_l != -1:  # '#'이 문자열에 있으면
+            lux = min(lux, i)  # 최소 인덱스 값 갱신
+            luy = min(luy, ix_l)
+            rdx = max(rdx, i)
+            rdy = max(rdy, ix_r)
+        print(lux, luy, rdx+1, rdy+1)
+        i += 1
+    print("final: ", lux, luy, rdx+1, rdy+1)
+    
+    return lux, luy, rdx+1, rdy+1
